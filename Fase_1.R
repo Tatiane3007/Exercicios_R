3279fefe461030bd5bc49aca8e51784c33a7bd1f
Tatiane Silva <50220046+Tatiane3007@users.noreply.github.com>
2021-06-01 18:14:35 -0300

    Create Fase_1.R

---
+++ Fase_1.R
@@ -0,0 +1,12 @@
+# Crie um algoritmo em R que mostre a mensagem "Alo mundo" na tela.
+
+print('Alo mundo')
+
+
+
+# Crie um algoritmo em R que peça dois números e imprima a soma.
+
+num1 = as.integer(readline("Digite um número: "))
+num2 = as.integer(readline("Digite outro número: "))
+soma = num1 + num2
+print(soma)
